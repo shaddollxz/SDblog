b9c68621c53db1c4aff8a402328233267afc6c35
ElShaddollxz <56341682+shaddollxz@users.noreply.github.com>
2022-06-12 16:10:53 +0800

    fix: 优化git存储

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,6 +3,7 @@ import "./utils/loadEnv"; // 加载环境变量
 import "./db/connect"; // 链接数据库
 import express from "express";
 import compression from "./middlewares/compression";
+import { resolve } from "path";
 
 const app = express();
 
@@ -10,8 +11,8 @@ app.use(compression); //gzip
 app.use(express.json());
 
 // 配置静态资源
-app.use("/", express.static(process.env.PUBLIC_DIST_PATH)); // 网页
-app.use("/assets", express.static(process.env.PUBLIC_STATIC_PATH)); // 上传的资源
+app.use("/", express.static(resolve(process.env.PUBLIC_DIST_PATH, "./dist"))); // 网页
+app.use("/assets", express.static(resolve(process.env.PUBLIC_STATIC_PATH, "./static"))); // 上传的资源
 
 // 配置接口
 import routes from "./routes";
