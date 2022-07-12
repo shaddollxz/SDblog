13072abc094bb3b92b42626bd6c84b57011736c9
ElShaddollxz <56341682+shaddollxz@users.noreply.github.com>
2022-07-12 21:32:57 +0800

    test: 新增单元测试

---
+++ packages/server/tests/isEmpty.test.ts
@@ -0,0 +1,18 @@
+import isEmpty from "../src/utils/isEmpty";
+
+describe("isEmpty", () => {
+    it("基础类型", () => {
+        expect(isEmpty(0)).toBe(false);
+        expect(isEmpty(0, true)).toBe(true);
+        expect(isEmpty("")).toBe(true);
+        expect(isEmpty(123)).toBe(false);
+    });
+    it("复杂类型", () => {
+        expect(isEmpty([])).toBe(true);
+        expect(isEmpty([[[]]])).toBe(true);
+        expect(isEmpty([0])).toBe(false);
+        expect(isEmpty([0], true)).toBe(true);
+        expect(isEmpty({})).toBe(true);
+        expect(isEmpty({ a: {}, b: {} })).toBe(true);
+    });
+});
