3b149bcb56f8ef88439ee283bcb0c0cc4b3628bb
ElShaddollxz <56341682+shaddollxz@users.noreply.github.com>
2022-09-01 17:04:11 +0800

    pref: 下载函数做了兼容优化

--- packages/client/src/utils/download.ts
+++ packages/client/src/utils/download.ts
@@ -21,12 +21,28 @@ export function download(filename: string, content: string | Blob) {
 /** 通过fetchApi下载大文件 */
 export async function downloadWithFetch(fileName: string, res: Response) {
     if (res.body) {
+        const readableStream = res.body!;
         const size = +res.headers.get("content-length")!;
         const fileStream = streamSaver.createWriteStream(fileName, { size });
-        res.body!.pipeTo(fileStream).then(() => {
-            fileStream.close();
-        });
-        return { size };
+        if (window.WritableStream && readableStream.pipeTo) {
+            await readableStream.pipeTo(fileStream);
+            return { size };
+        } else {
+            const writer = fileStream.getWriter();
+            const reader = res.body.getReader();
+            await pump();
+            return { size };
+
+            async function pump() {
+                const readerRes = await reader.read();
+                if (readerRes.done) {
+                    writer.close();
+                } else {
+                    await writer.write(readerRes.value);
+                    pump();
+                }
+            }
+        }
     } else {
         Message.error("文件下载失败");
         throw "";
